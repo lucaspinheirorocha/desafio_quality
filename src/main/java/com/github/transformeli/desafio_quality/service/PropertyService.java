b694aaeab95e6b594691c8506a01bf6aa37cf7c8
laridevmeli <larissa.navarro@mercadolivre.com>
2022-07-14 17:54:04 -0300

    ajustes

--- src/main/java/com/github/transformeli/desafio_quality/service/PropertyService.java
+++ src/main/java/com/github/transformeli/desafio_quality/service/PropertyService.java
@@ -2,6 +2,8 @@ package com.github.transformeli.desafio_quality.service;
 
 import com.github.transformeli.desafio_quality.dto.Property;
 import com.github.transformeli.desafio_quality.dto.Room;
+import com.github.transformeli.desafio_quality.exception.NotFoundException;
+import com.github.transformeli.desafio_quality.exception.NullPointerException;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -10,6 +12,21 @@ import java.util.concurrent.atomic.AtomicReference;
 @Service
 public class PropertyService implements IPropertyService {
 
+    public boolean validation(Property property){
+        if(property.getRooms().isEmpty() || property.getNeighborhood().equals(null)){
+            throw new NotFoundException("Same attributes not found ");
+        }
+        return true;
+
+    }
+    public boolean validation(Room room){
+        if(room.equals(null)){
+            throw new NotFoundException("Room not found.");
+        }
+        return true;
+
+    }
+
     /**
      * Total area calculator per room
      *
@@ -17,12 +34,16 @@ public class PropertyService implements IPropertyService {
      * @author Isaias Finger and Rebecca Cruz
      */
     public Double roomTotalArea(Room room) {
+        validation(room);
         return room.getLength() * room.getWidth();
     }
 
     public Double roomTotalArea(Property property)
     {
+        validation(property);
         return roomTotalArea(property.getRooms().stream().findFirst().get());
+
+
     }
 
     /**
@@ -32,6 +53,7 @@ public class PropertyService implements IPropertyService {
      * @author Isaias Finger and Rebecca Cruz
      */
     public Double propTotalArea(Property property) {
+        validation(property);
         AtomicReference<Double> total = new AtomicReference<>(0D);
         property.getRooms().stream()
                 .forEach(r -> total.updateAndGet(v -> v + roomTotalArea(r)));
@@ -45,6 +67,7 @@ public class PropertyService implements IPropertyService {
      * @author Rebecca Cruz and Isaias Finger
      */
     public Room propBiggestRoom(Property property) {
+        validation(property);
         return property.getRooms().stream().max(Comparator.comparing(r -> roomTotalArea(r))).get();
     }
 
@@ -55,6 +78,7 @@ public class PropertyService implements IPropertyService {
      * @author Rebecca Cruz and Isaias Finger
      */
     public Double propPriceByNeighborhood(Property property) {
+        validation(property);
         Double propArea = propTotalArea(property);
         Double sqPrice = property.getNeighborhood().getSqMeterPrice();
         return propArea * sqPrice;
--- src/test/java/com/github/transformeli/desafio_quality/service/PropertyServiceTest.java
+++ src/test/java/com/github/transformeli/desafio_quality/service/PropertyServiceTest.java
@@ -1,9 +1,18 @@
 package com.github.transformeli.desafio_quality.service;
 
+import com.github.transformeli.desafio_quality.dto.Neighborhood;
 import com.github.transformeli.desafio_quality.dto.Property;
 import com.github.transformeli.desafio_quality.dto.Room;
+import com.github.transformeli.desafio_quality.exception.NotFoundException;
+import com.github.transformeli.desafio_quality.util.TestUtilsProperty;
 import com.github.transformeli.desafio_quality.util.TestUtilsProperty2;
 import org.junit.jupiter.api.*;
+import org.mockito.internal.matchers.Not;
+import org.springframework.http.HttpStatus;
+
+import javax.validation.constraints.Null;
+
+import java.util.HashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,38 +26,66 @@ class PropertyServiceTest {
     }
 
     @Test
-    void roomTotalArea() {
-        Room room = TestUtilsProperty2.getNewRoom();
-
+    void roomTotalArea_returnArea_whenRoomExists() {
+        Room room = TestUtilsProperty.getNewRoom();
         Double result = service.roomTotalArea(room);
-
+        assertThat(result).isPositive();
+        assertThat(result).isNotNull();
         assertThat(result).isEqualTo(6D);
     }
 
     @Test
-    void propTotalArea() {
-        Property prop = TestUtilsProperty2.getNewProperty();
+    void propTotalArea_returnTotal_whenPropertyRoomExists() {
+        Property prop = TestUtilsProperty.getNewProperty();
+        Double result = service.roomTotalArea(prop);
+        assertThat(result).isNotNull();
+        assertThat(result).isPositive();
+        assertThat(result).isEqualTo(220D);
+    }
+    @Test
+    void propTotalArea_returnTotal_whenPropertyRoomNotExists() {
+        Property prop = TestUtilsProperty.getNewProperty();
+        prop.setRooms(new HashSet<>());
 
-        Double result = service.propTotalArea(prop);
+        NotFoundException ex = Assertions.assertThrows(NotFoundException.class, () -> {
+            service.roomTotalArea(prop);
+        });
+        assertThat(ex.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 
-        assertThat(result).isEqualTo(30D);
     }
 
     @Test
-    void propBiggestRoom() {
-        Property prop = TestUtilsProperty2.getNewProperty();
-
+    void propBiggestRoom_returnBiggestRoom_WhenRoomExist() {
+        Property prop = TestUtilsProperty.getNewProperty();
         Room result = service.propBiggestRoom(prop);
-
-        assertThat(result.getName()).isEqualTo("Test 2");
+        assertThat(result).isNotNull();
+        assertThat(result.getName()).isEqualTo("Sala");
     }
 
     @Test
-    void propPriceByNeighborhood() {
-        Property prop = TestUtilsProperty2.getNewProperty();
+    void propBiggestRoom_returnException_WhenRoomNotExist() {
+        Property prop = TestUtilsProperty.getNewProperty();
+        prop.setRooms(new HashSet<>());
+        NotFoundException ex = Assertions.assertThrows(NotFoundException.class, () -> {
+            Room result = service.propBiggestRoom(prop);
+        });
+        assertThat(ex.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 
+
+    }
+
+    @Test
+    void propPriceByNeighborhood_returnValue_WhenPropertyExists() {
+        Property prop = TestUtilsProperty.getNewProperty();
         Double result = service.propPriceByNeighborhood(prop);
+        assertThat(result).isEqualTo(9225D);
+        assertThat(result).isPositive();
 
-        assertThat(result).isEqualTo(900D);
+    }
+
+    @Test
+    void propPriceByNeighborhood_returnException_WhenPropertyNotExists() {
+
+
     }
 }
