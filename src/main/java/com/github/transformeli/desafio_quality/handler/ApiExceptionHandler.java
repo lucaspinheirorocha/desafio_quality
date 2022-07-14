fd65f737e76e12b44c4b5d7d052809d771d28cf1
Isaias <isaias.finger@mercadolivre.com>
2022-07-14 18:42:00 -0300

    feat: NeighborhoodController and refactor findByKey with Optional

--- src/main/java/com/github/transformeli/desafio_quality/handler/ApiExceptionHandler.java
+++ src/main/java/com/github/transformeli/desafio_quality/handler/ApiExceptionHandler.java
@@ -2,7 +2,10 @@ package com.github.transformeli.desafio_quality.handler;
 
 import com.github.transformeli.desafio_quality.dto.ApiExceptionDTO;
 import com.github.transformeli.desafio_quality.exception.ApiException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +15,22 @@ public class ApiExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiExceptionDTO> handlerApiException(ApiException ex)
     {
+        return new ResponseEntity<>(ex.getDTO(), ex.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiExceptionDTO> handlerMethodException(MethodArgumentNotValidException exception)
+    {
+        ApiException ex = new ApiException(exception.getFieldError().getDefaultMessage());
+        ex.setStatus(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(ex.getDTO(), ex.getStatus());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiExceptionDTO> handlerHttpMessageNotReadable(HttpMessageNotReadableException exception)
+    {
+        ApiException ex = new ApiException(exception.getLocalizedMessage());
+        ex.setStatus(HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(ex.getDTO(), ex.getStatus());
     }
 
