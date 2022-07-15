f7b1cd7ceaf8d6813d09a8af94d852d1dfba3c90
aborgessouzameli <alexandre.bsouza@mercadolivre.com>
2022-07-15 19:29:13 -0300

    <feat> created tests for Property Endpoint Controller

--- src/test/java/com/github/transformeli/desafio_quality/controller/PropertyControllerTest.java
+++ src/test/java/com/github/transformeli/desafio_quality/controller/PropertyControllerTest.java
@@ -2,12 +2,11 @@ package com.github.transformeli.desafio_quality.controller;
 
 import com.github.transformeli.desafio_quality.dto.Property;
 import com.github.transformeli.desafio_quality.dto.Room;
+import com.github.transformeli.desafio_quality.exception.NotFoundException;
 import com.github.transformeli.desafio_quality.service.IPropertyService;
-import com.github.transformeli.desafio_quality.service.PropertyService;
 import com.github.transformeli.desafio_quality.util.TestUtilsProperty;
 import com.github.transformeli.desafio_quality.util.TestUtilsProperty2;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,7 +20,12 @@ import org.mockito.quality.Strictness;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Map;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -29,17 +33,15 @@ class PropertyControllerTest {
 
     @InjectMocks
     private PropertyController controller;
+
     @Mock
     IPropertyService service;
 
-    @BeforeEach
-    public void setup() {
-        BDDMockito.when(service.propBiggestRoom(ArgumentMatchers.any(Property.class))).thenReturn(TestUtilsProperty2.propBiggestRoom());
-    }
-
     @Test
-    @DisplayName("Search for the biggest room inside the property")
-    public void propBiggestRoom() {
+    @DisplayName("Return successfully max room by property with contents")
+    void propBiggestRoom_returnSucessfully_whenWithPropertyContent() {
+        BDDMockito.when(service.propBiggestRoom(ArgumentMatchers.any(Property.class))).thenReturn(TestUtilsProperty2.propBiggestRoom());
+
         Property property = TestUtilsProperty2.getNewProperty();
         ResponseEntity<Room> response = controller.propBiggestRoom(property);
 
@@ -49,5 +51,112 @@ class PropertyControllerTest {
         assertThat(response.getBody().getWidth()).isPositive();
     }
 
-    
+    @Test
+    @DisplayName("Return falied max room by without property contents")
+    void propBiggestRoom_returnFalied_whenWithOutPropertyContent() {
+        BDDMockito.when(service.propBiggestRoom(ArgumentMatchers.any(Property.class))).thenReturn(TestUtilsProperty2.propBiggestNoRoom());
+
+        Property property = TestUtilsProperty.generateNewPropertyWhitoutContents();
+        ResponseEntity<Room> response = controller.propBiggestRoom(property);
+        assertTrue(response.getBody().getName() != null);
+        verify(service, atLeastOnce()).propBiggestRoom(property);
+    }
+
+    @Test
+    @DisplayName("Return success property price by neighborhood when property have contents")
+    void propPriceByNeighborhood_returnSuccessfully_whenPropertyHaveContents() {
+        BDDMockito.when(service.propPriceByNeighborhood(ArgumentMatchers.any(Property.class))).thenReturn(TestUtilsProperty.propPriceByNeighborhoodWithValue());
+
+        Property property = TestUtilsProperty2.getNewProperty();
+        ResponseEntity<Map<Property, Double>> response = controller.propPriceByNeighborhood(property);
+        assertThat(response.getBody()).isNotNull();
+        assertFalse(response.getBody().entrySet().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Return faliure property price by neighborhood when property not have contents")
+    void propPriceByNeighborhood_returnSuccessfully_whenPropertyNotHaveContents() throws NotFoundException {
+        BDDMockito.when(service.propPriceByNeighborhood(ArgumentMatchers.any(Property.class))).thenReturn(TestUtilsProperty.propPriceByNeighborhoodWithoutValue());
+        Property property = TestUtilsProperty.generateNewPropertyWhitoutContents();
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            throw new NotFoundException("Room not found.");
+        });
+        assertEquals("Room not found.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Return successfully created a new property")
+    public void createProperty_returnSuccessfully_whenWithPropertyContent() {
+        BDDMockito.when(service.createNewProperty(ArgumentMatchers.any(Property.class))).thenReturn((TestUtilsProperty.getNewProperty()));
+        Property newProperty = TestUtilsProperty2.getNewProperty();
+        ResponseEntity<Property> response = controller.createNewNProperty(newProperty);
+        assertThat(response.getBody()).isInstanceOf(Property.class);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getRooms().size())
+                .isNotNull()
+                .isPositive();
+        verify(service, atLeastOnce()).createNewProperty(newProperty);
+    }
+
+    @Test
+    @DisplayName("Return falied created a new property")
+    public void createProperty_returnFalied_whenPropertyNoContent() {
+        BDDMockito.when(service.createNewProperty(ArgumentMatchers.any(Property.class))).thenReturn((TestUtilsProperty.generateNewPropertyWhitoutContents()));
+
+        Property newProperty = TestUtilsProperty.generateNewPropertyWhitoutContents();
+        ResponseEntity<Property> response = controller.createNewNProperty(newProperty);
+        assertTrue(response.getBody().getName().isEmpty());
+        assertFalse(response.getBody().getRooms() != null);
+        assertFalse(response.getBody().getNeighborhood() != null);
+    }
+
+    @Test
+    @DisplayName("Return successfully updated property")
+    public void updateProperty_returnSuccessfully_whenWithPropertyContent() {
+        BDDMockito.when(service.updateProperty(ArgumentMatchers.anyString(), ArgumentMatchers.any(Property.class))).thenReturn((TestUtilsProperty.getNewProperty()));
+
+        String name = "Casa da Lari";
+        Property newProperty = TestUtilsProperty.getNewProperty();
+        ResponseEntity<Property> response = controller.updateNProperty(newProperty,name);
+        Assertions.assertThat(response.getBody()).isInstanceOf(Property.class);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getRooms().size())
+                .isNotNull()
+                .isPositive();
+        verify(service, atLeastOnce()).updateProperty(name, newProperty);
+    }
+
+    @Test
+    @DisplayName("Return falied updated property")
+    public void updateProperty_returnFalied_whenWithoutPropertyContent() {
+        BDDMockito.when(service.updateProperty(ArgumentMatchers.anyString(), ArgumentMatchers.any(Property.class))).thenReturn((TestUtilsProperty.getNewProperty()));
+
+        String name = "Mansão do Faustão";
+        Property newProperty = TestUtilsProperty.getNewProperty();
+        ResponseEntity<Property> response = controller.updateNProperty(newProperty, name);
+        assertTrue(response.getBody().getName().isEmpty());
+        assertFalse(response.getBody().getRooms() != null);
+        assertFalse(response.getBody().getNeighborhood() != null);
+    }
+    @Test
+    @DisplayName("Return successfully delete property")
+    void deleteProperty_returnSucessfully_whenWithPropertyContent() {
+
+        BDDMockito.when(service.deleteProperty(ArgumentMatchers.anyString())).thenReturn(true);
+        String name = TestUtilsProperty.getNewProperty().getName().toString();
+
+        ResponseEntity<Void> response = controller.deleteProperty(name);
+        verify(service, atLeastOnce()).deleteProperty(name);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    @DisplayName("Return successfully delete property")
+    void deleteProperty_returnFalied_whenWithoutPropertyContent() {
+        BDDMockito.when(service.deleteProperty(ArgumentMatchers.anyString())).thenReturn(false);
+
+        String name = "Mansão do Faustão";
+        ResponseEntity<Void> response = controller.deleteProperty(name);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    }
 }
