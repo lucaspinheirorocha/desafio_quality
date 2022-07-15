c55d5c47dbbc1d2381781541fbab8852fae461a8
Isaias <isaias.finger@mercadolivre.com>
2022-07-15 14:20:14 -0300

    fix: Property name validation now using NotEmpty, fix: regex and remove unused methods

--- src/main/java/com/github/transformeli/desafio_quality/dto/Property.java
+++ src/main/java/com/github/transformeli/desafio_quality/dto/Property.java
@@ -4,31 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-import java.util.Optional;
-import java.util.Set;
+import javax.validation.*;
+import javax.validation.constraints.*;
+import java.util.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Property {
-    @NotBlank(message = "O nome da propriedade não pode estar vazio!")
-    @Pattern(regexp = "\"([A-Z]|[0-9])[\\\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$\"", message = "O nome da propriedade deve comecar com uma letra maíuscula!")
+    @NotEmpty(message = "O nome da propriedade não pode estar vazio!")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "O nome da propriedade deve iniciar com uma letra maíuscula!")
     @Size(max= 30, message = "O comprimento do nome não pode exceder trinta caracteres!")
     private String name;
+    @NotEmpty(message = "A lista de cômodos deve ter pelo menos 1 válido.")
     private Set<@Valid Room> rooms;
     private @Valid Neighborhood neighborhood;
-
-    public double propertyDimensions(){
-        return 0.0;
-    }
-    public double totalCost(){
-        return 0.0;
-    }
-
 }
