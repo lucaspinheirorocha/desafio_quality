45731cc2ba93ca352d114029d543455296000bb8
Isaias <isaias.finger@mercadolivre.com>
2022-07-15 14:21:10 -0300

    fix: Room name validation now using NotEmpty, fix: regex, fix decimal validation

--- src/main/java/com/github/transformeli/desafio_quality/dto/Room.java
+++ src/main/java/com/github/transformeli/desafio_quality/dto/Room.java
@@ -4,23 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Room {
-    @NotBlank(message = "O nome da propriedade não pode estar vazio.")
-    @Pattern(regexp = "\"([A-Z]|[0-9])[\\\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$\"", message = "O nome do cômodo deve comecar com uma letra maíuscula.")
+    @NotEmpty(message = "O nome do cômodo não pode estar vazio.")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|[0-9]|A-Z|a-z|ñ|ó|í|á|é|ú|Á|Ó|É|Í|Ú]*$", message = "O nome do cômodo deve iniciar com uma letra maíuscula.")
     @Size(max= 30, message = "O comprimento do nome não pode exceder trinta caracteres.")
     private String name;
-    @NotBlank(message = "O comprimento do cômodo não pode estar vazio.")
+    @NotNull(message = "O comprimento do cômodo não pode estar vazio.")
+    @DecimalMin(value = "0.0", message = "O comprimento mínimo por cômodo.")
     @DecimalMax(value = "33.0", message = "O comprimento máximo permitido por cômodo é de 33 metros.")
     private Double length;
-    @NotBlank(message = "A largura do cômodo não pode estar vazio.")
+    @NotNull(message = "A largura do cômodo não pode estar vazio.")
+    @DecimalMin(value = "0.0", message = "A largura mínima por cômodo.")
     @DecimalMax(value = "25.0", message = "A largura máxima permitida por cômodo é de 25 metros.")
     private Double width;
 }
