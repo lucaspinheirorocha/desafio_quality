ded8fc0c6ae3fbf2ef4530232e32d207a23b6b73
Isaias <isaias.finger@mercadolivre.com>
2022-07-15 22:45:49 -0300

    feat: FindAll and FindOne Property with calculated values

--- src/main/java/com/github/transformeli/desafio_quality/service/IPropertyService.java
+++ src/main/java/com/github/transformeli/desafio_quality/service/IPropertyService.java
@@ -1,13 +1,22 @@
 package com.github.transformeli.desafio_quality.service;
 
 import com.github.transformeli.desafio_quality.dto.Property;
+import com.github.transformeli.desafio_quality.dto.PropertyDTO;
 import com.github.transformeli.desafio_quality.dto.Room;
+import com.github.transformeli.desafio_quality.dto.RoomDTO;
+
+import java.util.Optional;
+import java.util.Set;
 
 public interface IPropertyService {
     Double roomTotalArea(Room room);
     Double propTotalArea(Property property);
     Room propBiggestRoom(Property property);
     Double propPriceByNeighborhood(Property property);
+    RoomDTO roomAreaCalculator(Room room);
+    PropertyDTO propAreaCalculator(Property prop);
+    Set<Property> findAll();
+    Optional<Property> findByKey(String name);
     Property createNewProperty(Property property);
     Property updateProperty(String name, Property property);
     Boolean deleteProperty(String name);
